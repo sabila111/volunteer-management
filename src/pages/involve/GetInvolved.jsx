a9dd01cd4df793ffa257b732702a92b59d844dda
sabila111 <sharminsharker2002@gmail.com>
2025-02-19 21:00:25 +0600

    finished all the work

--- src/pages/involve/GetInvolved.jsx
+++ src/pages/involve/GetInvolved.jsx
@@ -10,7 +10,7 @@ const GetInvolved = () => {
       </div>
 
       {/* Contribution Cards */}
-      <div className="grid md:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* Donate */}
         <div className="p-8 rounded-2xl shadow-lg text-center border hover:shadow-xl transition-all">
           <FaHandHoldingHeart className="text-5xl text-red-500 mx-auto" />
